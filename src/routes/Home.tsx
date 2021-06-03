cf3c0119898b5cf4b32bf9a43570a85ca4707b5b
Ryan <orion31dev@gmail.com>
2021-06-03 10:55:44 -0400

    Whispers of Spring: When the Goats Come Out to Play.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import fallImg from '../fall.png';
+import summerImg from '../summer.png';
 
 export default function Home() {
   let [scroll, setScroll] = useState(0);
@@ -22,15 +23,39 @@ export default function Home() {
       <div className="section" id="title-section">
         <div className="title">SUMMER 2021</div>
       </div>
-      <div className="section" id="fall-section">
+      <div className="section" id="summer-section">
         <div className="section-header">&gt; THE SUMMER COLLECTION</div>
         <div className="section-big-desc">
           NEW. <br />
           CLASSY. <br />
-          GOATS.  
+          GOATS.
         </div>
 
-        {/* Inefficient but whatever, also imagine commenting jsx */}
+        {/* Inefficient but whatever, also imagine commenting in jsx */}
+        <div className="img-decal">
+          FEAR OF GOATS <br />
+          FEAR OF GOATS <br />
+          FEAR OF GOATS <br />
+          FEAR OF GOATS <br />
+          FEAR OF GOATS <br />
+          FEAR OF GOATS <br />
+          FEAR OF GOATS <br />
+          FEAR OF GOATS <br />
+          FEAR OF GOATS <br />
+          FEAR OF GOATS
+        </div>
+
+        <img src={summerImg} alt="hoodie" className="section-img" id="summer-img" />
+      </div>
+      <div className="section" id="fall-section">
+        <div className="section-header">&gt; THE FALL COLLECTION</div>
+        <div className="section-big-desc">
+          UNIQUE. <br />
+          DEFINITIVE. <br />
+          GOATS.
+        </div>
+
+        {/* Inefficient but whatever, also imagine commenting in jsx */}
         <div className="img-decal">
           FEAR OF GOATS <br />
           FEAR OF GOATS <br />
@@ -45,6 +70,14 @@ export default function Home() {
         </div>
 
         <img src={fallImg} alt="hoodie" className="section-img" id="fall-img" />
+      </div>
+      <div className="section" id="spring-section">
+        <div className="section-header">&gt; WHISPERS OF SPRING</div>
+        <div className="spring-decal">WHISPERS OF SPRING WHISPERS OF SPRING WHISPERS OF SPRING</div>
+        <div className="spring-decal s1">WHISPERS OF SPRING WHISPERS OF SPRING WHISPERS OF SPRING</div>
+        <div className="spring-decal s2">WHISPERS OF SPRING WHISPERS OF SPRING WHISPERS OF SPRING</div>
+
+        <div className="center">THE GOATS COME OUT TO PLAY</div>
       </div>
       {scroll > window.innerHeight / 2 && <Footer opacity={(scroll - window.innerHeight / 2) / (window.innerHeight / 6)} />}
     </div>
