9417ed6c9f4a9d94441a11659cfaf467b0d079d8
moyni-han <connman1220@gmail.com>
2021-06-04 14:51:48 -0400

    Reverted Past Changes and added broken countdown timer

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -11,6 +11,32 @@ export default function Home() {
       console.log('eee');
       console.log(scroll);
       setScroll(window.scrollY);
+//       // Set the date we're counting down to
+// var countDownDate = new Date("July 5, 2021 15:37:25").getTime();
+
+// // Update the count down every 1 second
+// var x = setInterval(function() {
+
+//   var now = new Date().getTime();
+    
+  
+//   var distance = countDownDate - now;
+    
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+//   // Output the result in an element with id="demo"
+
+//   document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    
+//   // If the count down is over, write some text 
+//   if (distance < 0) {
+//     clearInterval(x);
+//   }
+// }, 1000);
     }
 
     console.log('e');
@@ -26,7 +52,7 @@ export default function Home() {
         <div className="title">SUMMER 2021</div>
       </div>
       <div className="section" id="summer-section">
-        <div className="section-header">&gt; 
+        <div className="section-header">&gt; THE SUMMER COLLECTION
         </div>
         <div className="section-big-desc">
           NEW. <br />
@@ -51,6 +77,8 @@ export default function Home() {
         <img src={summerImg} alt="skateboarder" className="section-img" id="summer-img" />
       </div>
       <div className="section" id="fall-section">
+      <div className="section-header">&gt; THE FALL COLLECTION</div>
+
         <div className="section-big-desc">
           UNIQUE. <br />
           DEFINITIVE. <br />
@@ -74,13 +102,15 @@ export default function Home() {
         <img src={fallImg} alt="hoodie" className="section-img" id="fall-img" />
       </div>
       <div className="section" id="spring-section">
-        <div className="section-header">&gt; REMNANTS OF SPRING</div>
+        <div className="section-header">&gt; WHISPERS OF SPRING</div>
         <div className="spring-decal">WHISPERS OF SPRING WHISPERS OF SPRING WHISPERS OF SPRING</div>
         <div className="spring-decal s1">WHISPERS OF SPRING WHISPERS OF SPRING WHISPERS OF SPRING</div>
         <div className="spring-decal s2">WHISPERS OF SPRING WHISPERS OF SPRING WHISPERS OF SPRING</div>
 
         <div className="center">THE GOATS COME OUT TO PLAY</div>
-        <div className = "centerbaby">JUNE 31, 14:28</div>
+        <div className = "centerbaby">
+        <p className="demo">TIMER HERE</p>
+        </div>
       </div>
       {scroll > window.innerHeight / 2 && <Footer opacity={(scroll - window.innerHeight / 2) / (window.innerHeight / 6)} />}
     </div>
